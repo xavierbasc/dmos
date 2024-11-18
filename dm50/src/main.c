11386d09cc7a761686bf7a8e813431a26feaa2a9
Javier Bascones <javier.bascones@gmail.com>
2024-11-18 21:00:40 +0100

    fullscreen iOS bugfix

--- dm50/src/main.c
+++ dm50/src/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 #include "SDL.h"
 #include "dm50_skin.h"
@@ -81,29 +82,32 @@ int main(int argc, char* argv[])
                "SDL_Error: %s\n", SDL_GetError());
         return 0;
     }
-
-    // Obtén el tamaño de la pantalla
-    SDL_DisplayMode displayMode;
-    if (SDL_GetCurrentDisplayMode(0, &displayMode) != 0) {
-        fprintf(stderr, "Error getting display mode: %s\n", SDL_GetError());
-        SDL_Quit();
-        return 1;
-    }
     
-    int screenWidth = displayMode.w;
-    int screenHeight = displayMode.h;
+    int screenWidth;
+    int screenHeight;
+    int windowWidth;
+    int windowHeight;
 
-    // Calcula la escala máxima manteniendo la proporción 50x170
-    int scaleX = screenWidth * 0.80 / SCREEN_WIDTH;
-    int scaleY = screenHeight * 0.80 / SCREEN_HEIGHT;
-    int scale = (scaleX < scaleY) ? scaleX : scaleY;
-
-    // Asegura una escala mínima de 1 para evitar problemas
-    if (scale < 1) scale = 1;
-
-    // Define el tamaño de la ventana escalada
-    int windowWidth = SCREEN_WIDTH * scale;
-    int windowHeight = SCREEN_HEIGHT * scale;
+    int scaleX, scaleY;
+    
+    if (strcmp(SDL_GetPlatform(), "iOS") == 0) {
+        scaleX = screenWidth / SCREEN_WIDTH;
+        scaleY = screenHeight / SCREEN_HEIGHT;
+    } else {
+        // Obtén el tamaño de la pantalla
+        SDL_DisplayMode displayMode;
+        if (SDL_GetCurrentDisplayMode(0, &displayMode) != 0) {
+            fprintf(stderr, "Error getting display mode: %s\n", SDL_GetError());
+            SDL_Quit();
+            return 1;
+        }
+        screenWidth = displayMode.w;
+        screenHeight = displayMode.h;
+        
+        // Calcula la escala máxima manteniendo la proporción 50x170
+        scaleX = screenWidth * 0.80 / SCREEN_WIDTH;
+        scaleY = screenHeight * 0.80 / SCREEN_HEIGHT;
+    }
 
 #if defined linux && SDL_VERSION_ATLEAST(2, 0, 8)
     // Disable compositor bypass
@@ -113,13 +117,30 @@ int main(int argc, char* argv[])
         return 0;
     }
 #endif
+    SDL_Window *window;
 
-    // Create window
-    SDL_Window *window = SDL_CreateWindow("DM50",
-                                          SDL_WINDOWPOS_UNDEFINED,
-                                          SDL_WINDOWPOS_UNDEFINED,
-                                          windowWidth, windowHeight,
-                                          SDL_WINDOW_SHOWN | SDL_WINDOW_ALLOW_HIGHDPI);
+    if (strcmp(SDL_GetPlatform(), "iOS") == 0) {
+        window = SDL_CreateWindow(NULL,
+            SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+            0, 0, SDL_WINDOW_FULLSCREEN|SDL_WINDOW_ALLOW_HIGHDPI
+        );
+    } else {
+        int scale = (scaleX < scaleY) ? scaleX : scaleY;
+
+        // Asegura una escala mínima de 1 para evitar problemas
+        if (scale < 1) scale = 1;
+
+        // Define el tamaño de la ventana escalada
+        windowWidth = SCREEN_WIDTH * scale;
+        windowHeight = SCREEN_HEIGHT * scale;
+
+        window = SDL_CreateWindow("DM50",
+              SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+              windowWidth, windowHeight,
+              SDL_WINDOW_SHOWN | SDL_WINDOW_ALLOW_HIGHDPI);
+    }
+    
+    
     if(!window)
     {
         printf("Window could not be created!\n"
@@ -127,24 +148,29 @@ int main(int argc, char* argv[])
     }
     else
     {
-        SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
         SDL_GetWindowSize(window, &screenWidth, &screenHeight);
+        SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
         
-        SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
-        SDL_RenderSetIntegerScale(renderer, SDL_TRUE);
+        if (strcmp(SDL_GetPlatform(), "iOS") == 0) {
+            //SDL_RenderSetLogicalSize(renderer, screenWidth, screenHeight);
+            SDL_GetRendererOutputSize(renderer, &screenWidth, &screenHeight);
+           // SDL_RenderSetIntegerScale(renderer, SDL_TRUE);
 
-        /*
-        SDL_Texture* texture = load_png("assets/dm50_skin.png", renderer);
-        if (!texture) {
-            fprintf(stderr, "Error al cargar la textura PNG.\n");
-            SDL_DestroyRenderer(renderer);
-            SDL_DestroyWindow(window);
-            SDL_Quit();
-            return -1;
+            // PIXEL PERFECT
+            SDL_RenderSetIntegerScale(renderer, SDL_TRUE);
+            int scaleX = screenWidth / SCREEN_WIDTH;
+            int scaleY = screenHeight /SCREEN_HEIGHT;
+            SDL_RenderSetScale(renderer, scaleX, scaleX);
+            SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH*scaleX, SCREEN_HEIGHT*scaleX);
+
+        } else {
+            SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
+            SDL_RenderSetIntegerScale(renderer, SDL_TRUE);
         }
-        */
 
+
+        
+        
         SDL_Texture* texture = load_img_from_memory(dm50_skin_gif, dm50_skin_gif_len, renderer);
 
         if (!texture) {
@@ -163,20 +189,12 @@ int main(int argc, char* argv[])
         }
         else
         {
-            // Declare rect of square
-            SDL_Rect squareRect;
-
-            // Square dimensions: Half of the min(SCREEN_WIDTH, SCREEN_HEIGHT)
-            squareRect.w = 128;
-            squareRect.h = 64;
-
-            // Square position: In the middle of the screen
-            squareRect.x = 1;
-            squareRect.y = 1;
-
             // Event loop exit flag
             bool quit = false;
 
+            int width, height;
+            SDL_GetRendererOutputSize(renderer, &width, &height);
+            
             // Event loop
             while(!quit)
             {
@@ -192,14 +210,23 @@ int main(int argc, char* argv[])
                 }
 
                 
-                SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF); // White
+                //SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF); // White
+                SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF); // White
                 SDL_RenderClear(renderer); // Clear screen
-                SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF); // Red
 
+                SDL_SetRenderDrawColor(renderer, 0xFF, 0x00, 0x00, 0xFF); // Red
                 SDL_RenderCopy(renderer, texture, NULL, NULL);
                 
-                SDL_SetRenderDrawColor(renderer, 196, 191, 185, 0xFF); // white
-                SDL_RenderFillRect(renderer, &squareRect); // display zone
+                /*
+                SDL_SetRenderDrawColor(renderer, 0, 255, 0, 0xFF);
+                SDL_Rect rect2 = {1, 1, screenWidth, screenHeight};
+                SDL_RenderFillRect(renderer, &rect2);
+                */
+
+                // LCD
+                SDL_SetRenderDrawColor(renderer, 196, 191, 185, 0xFF);
+                SDL_Rect rect = {1, 1, 128, 64};
+                SDL_RenderFillRect(renderer, &rect);
 
                 SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 0xFF); // White
                 draw_text(renderer, ">01234567890123456789", 1, 1);
