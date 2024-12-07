5f4e72349930920537efbd20fb4fefcb15d5b0d1
nixuser <nixuser1980@gmail.com>
2024-12-07 08:28:49 +0800

    Added zero button press detection

--- dm50/src/keyboard.c
+++ dm50/src/keyboard.c
@@ -6,6 +6,10 @@ KeyPress getKeyPress(int x, int y)
     { 
         return ALL_CLEAR;
     }
+    else if (ZERO_LEFT <= x && x <= ZERO_LEFT + ZERO_WIDTH && ZERO_TOP <= y && y <= ZERO_TOP + ZERO_HEIGHT)    
+    {
+        return ZERO;
+    }
     
     return UNDEFINED; 
 }
--- dm50/src/keyboard.h
+++ dm50/src/keyboard.h
@@ -2,11 +2,18 @@
 #define KEYBOARD_H
 
 #define AC_LEFT 8
-#define AC_TOP 518
 #define AC_WIDTH 32
+#define AC_TOP 518
 #define AC_HEIGHT 32
 
-typedef enum { UNDEFINED = -1, ALL_CLEAR } KeyPress;
+#define ZERO_LEFT 64
+#define ZERO_WIDTH 40
+#define ZERO_TOP 518
+#define ZERO_HEIGHT 32
+
+typedef enum { UNDEFINED = -1,
+               ALL_CLEAR,
+               ZERO } KeyPress;
 
 KeyPress getKeyPress(int x, int y);
 
