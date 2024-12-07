1cff90df7b5c0b97446d8cb0295cc62904bd8e9e
nixuser <nixuser1980@gmail.com>
2024-12-07 09:00:20 +0800

    First row of keypress event detection implemented

--- dm50/src/keyboard.c
+++ dm50/src/keyboard.c
@@ -10,6 +10,18 @@ KeyPress getKeyPress(int x, int y)
     {
         return ZERO;
     }
+    else if (DECIMAL_POINT_LEFT <= x && x <= DECIMAL_POINT_LEFT + DECIMAL_POINT_WIDTH && DECIMAL_POINT_TOP <= y && y <= DECIMAL_POINT_TOP + DECIMAL_POINT_HEIGHT)    
+    {
+        return DECIMAL_POINT;
+    }
+    else if (EQUALS_LEFT <= x && x <= EQUALS_LEFT + EQUALS_WIDTH && EQUALS_TOP <= y && y <= EQUALS_TOP + EQUALS_HEIGHT)    
+    {
+        return EQUALS;
+    }
+    else if (PLUS_LEFT <= x && x <= PLUS_LEFT + PLUS_WIDTH && PLUS_TOP <= y && y <= PLUS_TOP + PLUS_HEIGHT)    
+    {
+        return PLUS;
+    }
     
     return UNDEFINED; 
 }
--- dm50/src/keyboard.h
+++ dm50/src/keyboard.h
@@ -6,14 +6,32 @@
 #define AC_TOP 518
 #define AC_HEIGHT 32
 
-#define ZERO_LEFT 64
+#define ZERO_LEFT 60
 #define ZERO_WIDTH 40
 #define ZERO_TOP 518
 #define ZERO_HEIGHT 32
 
+#define DECIMAL_POINT_LEFT 110
+#define DECIMAL_POINT_WIDTH 40
+#define DECIMAL_POINT_TOP 518
+#define DECIMAL_POINT_HEIGHT 32
+
+#define EQUALS_LEFT 160
+#define EQUALS_WIDTH 40
+#define EQUALS_TOP 518
+#define EQUALS_HEIGHT 32
+
+#define PLUS_LEFT 210
+#define PLUS_WIDTH 40
+#define PLUS_TOP 518
+#define PLUS_HEIGHT 32
+
 typedef enum { UNDEFINED = -1,
                ALL_CLEAR,
-               ZERO } KeyPress;
+               ZERO,
+               DECIMAL_POINT,
+               EQUALS,
+               PLUS } KeyPress;
 
 KeyPress getKeyPress(int x, int y);
 
